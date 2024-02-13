205494c7255723069f3421ee68851d1ef8be92e8
김치헌 <chk87a@gmail.com>
2024-02-13 19:32:41 +0900

    Revert "Merge branch 'MapDesign' into Temp"
    This reverts commit bc5b9cdba0aac0c8cedd701b56c863f62e35927d, reversing
    changes made to 85d1af545f369a9f40c0106578d5a069dffb940b.

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    public event Action OnLoopEvent;
     public GameObject player;
-
 
     private void Awake()
     {
@@ -31,24 +28,9 @@ public class GameManager : MonoBehaviour
     {
         if (player.transform.position.z > 14)
         {
-            if (player.transform.position.x >= 15.5f && player.transform.position.y >= 4.2f)
-            {
-                player.transform.position = new Vector3(player.transform.position.x, -3.65f, player.transform.position.z);
-                CallLoopEvent();
-            }
-
-            else if (player.transform.position.x <= 15.4f && player.transform.position.y <= -3.65f)
-            {
-                player.transform.position = new Vector3(player.transform.position.x, 4.3f, player.transform.position.z);
-                CallLoopEvent();
-            }
+            if (player.transform.position.x >= 16.5f&&player.transform.position.y>=4.2f) player.transform.position = new Vector3(player.transform.position.x, -3.65f, player.transform.position.z);
+            else if (player.transform.position.x <= 16.4f && player.transform.position.y <= -3.65f) player.transform.position = new Vector3(player.transform.position.x, 4.3f, player.transform.position.z);
         }
 
     }
-    private void CallLoopEvent()
-    {
-        OnLoopEvent?.Invoke();
-    }
-
-
 }
