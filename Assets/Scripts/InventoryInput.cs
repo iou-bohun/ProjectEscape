6093dd560c8585aad5d4da00392e05b993dfd9ae
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-02-13 10:05:09 +0900

    Feat - 아이템 선택

--- Assets/Scripts/InventoryInput.cs
+++ Assets/Scripts/InventoryInput.cs
@@ -7,14 +7,7 @@ using UnityEngine.InputSystem;
 public class InventoryInput : MonoBehaviour
 {
     int currentIndex = -1;
-    public void ItemChoiceInput(InputAction.CallbackContext context)
-    {
-        //인벤값은 1~4 하지만 인덱스로 적용할시 0~3이기에 값을받고 -1해줄 필요성이 있음
-        //0~3은 아이템 칸 -1은 빈칸으로 설정
-        //아이템 키패드를 선택하면 선택한 것에 따라 currentitem을 설정할 예정
-        //currentitem과 같은 값이 나오면 다시 -1을 해줌
-        //빈칸의 키를 선택할시 적용할 방법 생각
-    }
+
     public void DropItemInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started && currentIndex != -1)
@@ -23,4 +16,42 @@ public class InventoryInput : MonoBehaviour
             currentIndex = -1;
         }
     }
+    #region Input
+    public void Item1Input(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            currentIndex = 0;
+        }
+        Hand();
+    }
+    public void Item2Input(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            currentIndex = 1;
+        }
+        Hand();
+    }
+    public void Item3Input(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            currentIndex = 2;
+        }
+        Hand();
+    }
+    public void Item4Input(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            currentIndex = 3;
+        }
+        Hand();
+    }
+    void Hand()
+    {
+        Inventory.i.CatchItem(currentIndex);
+    }
+    #endregion
 }
