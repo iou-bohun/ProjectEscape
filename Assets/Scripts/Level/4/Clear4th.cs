7f4742f20adf9d768772b6b081eb9e06dd78264c
bohun <slawkd2002@gmail.com>
2024-02-16 01:41:10 +0900

    Feat: Clear4thScene

---
+++ Assets/Scripts/Level/4/Clear4th.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clear4th : MonoBehaviour
+{
+    [SerializeField] IsOrigiPosition[] origins;
+    private int clearCount =0;
+
+   
+
+    private void CheckClear()
+    {
+       for(int i = 0; i < origins.Length; i++)
+        {
+            if (origins[i].isOrigin == true)
+            {
+                clearCount++;   
+            }
+        }
+        if (clearCount >= 1)
+        {
+            GameManager.Instance.Clear();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            CheckClear();
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            clearCount = 0;
+        }
+    }
+}
