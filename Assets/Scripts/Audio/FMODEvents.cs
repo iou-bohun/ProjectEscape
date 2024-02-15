2da8726c20e535db53c9894fb8bfdec8bffca210
najdorf <70437537+cn7249@users.noreply.github.com>
2024-02-15 11:58:46 +0900

    Feat : 창문 관련 오디오 이벤트 추가
    One-Shot : WindowOpened, WindowClosed
    Loop : WindowRattle

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -16,6 +16,12 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference doorOpened { get; private set; }
     [field: SerializeField] public EventReference doorClosed { get; private set; }
 
+
+    [field: Header("Window SFX")]
+    [field: SerializeField] public EventReference windowOpened { get; private set; }
+    [field: SerializeField] public EventReference windowClosed { get; private set; }
+    [field: SerializeField] public EventReference windowRattle { get; private set; }
+
     public static FMODEvents instance {  get; private set; }
 
     private void Awake()
