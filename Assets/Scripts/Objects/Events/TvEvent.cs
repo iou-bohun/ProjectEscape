f316999fbe8822948b45924c4df7ceae85ce2c78
najdorf <70437537+cn7249@users.noreply.github.com>
2024-02-15 17:02:12 +0900

    Feat : TV SFX 추가

--- Assets/Scripts/Objects/Events/TvEvent.cs
+++ Assets/Scripts/Objects/Events/TvEvent.cs
@@ -1,22 +1,30 @@
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
+[RequireComponent(typeof(StudioEventEmitter))]
 public class TvEvent : MonoBehaviour
 {
     [SerializeField] private VideoPlayer _videoPlayer;
+
+    // Audio
+    private StudioEventEmitter emitter;
 
     private void Start()
     {
         _videoPlayer = GetComponent<VideoPlayer>();
         EventManager.I.livingRoomEvent += PlayVideo;
+        emitter = AudioManager.instance.InitializeEventEmitter(FMODEvents.instance.TVStatic, this.gameObject);
     }
 
     private void PlayVideo()
     {
         _videoPlayer.enabled = true;
         _videoPlayer.Play();
+        emitter.SetParameter("TVEnd", 0.1f);
+        emitter.Play();
         StartCoroutine(OffVideo()); 
     }
 
@@ -24,5 +32,8 @@ public class TvEvent : MonoBehaviour
     {
         yield return new WaitForSeconds(10f);
         _videoPlayer.enabled = false;
+        emitter.SetParameter("TVEnd", 1.0f);
+        yield return new WaitForSeconds(3f);
+        emitter.Stop();
     }
 }
