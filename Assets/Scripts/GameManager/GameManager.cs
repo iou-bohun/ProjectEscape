164be13591b78b35c366d7e6d11612c64206e35a
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-02-16 03:49:16 +0900

    a

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     public GameObject DiePanel;
     WaitForSeconds waitSceneTime;
     public GameObject completePaper;
-    public int stage;
+    int stage;
 
     [Header("boolCheck")]
     public bool isCanGoRoofTop = false;
